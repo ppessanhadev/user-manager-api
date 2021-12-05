bfe35bb16ad36645a9f7011b60ca6cb385e7b217
Pablo Pessanha <pablopessanh@gmail.com>
2021-12-05 18:21:55 -0300

    feat: add server routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import User from './models/User';
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (_req, res) => {
-  return res.status(200).json({ message: 'ok' });
+app.get('/', async (_req, res) => {
+  const response = await User.getUsers();
+  return res.status(200).json(response);
+});
+
+app.post('/', async (req, res) => {
+  const response = await User.newUser(req.body);
+  return res.status(201).json(response);
+});
+
+app.put('/', async (req, res) => {
+  const response = await User.updateUser(req.body);
+  return res.status(200).json({ response });
+});
+
+app.delete('/', async (req, res) => {
+  const id = Number(req.query.userId);
+  const response = await User.deleteUser(id);
+  return res.status(204).json(response);
 });
 
 app.listen(3333);
